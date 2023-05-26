9231cab4de54a7a19f8a7cb4c33456f43fbe98a9
orekbaby <orekoyafolakemi@gmail.com>
2023-05-26 18:58:51 +0100

    chore: initial setup

---
+++ index.js
@@ -0,0 +1,48 @@
+document.getElementById("myForm").addEventListener("submit", function(event) {
+  
+
+  let nameInput = document.getElementById("name");
+    let emailInput = document.getElementById("email");
+    
+ 
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let cards = document.querySelectorAll(".card"); // Select all card elements
+    if (!nameInput.value || !emailPattern.test(emailInput.value)) {
+      event.preventDefault(); // Prevent form submission
+      alert("Please fill in all required fields and enter a valid email address.");
+    }
+  });
+
+
+// cards function
+let cards = document.querySelectorAll(".card");
+  cards.forEach(function(card) {
+    card.addEventListener("click", function() {
+      
+      cards.forEach(function(card) {card.classList.remove("active");
+     });
+      // Add active class to the clicked card
+     card.classList.add("active");
+   });
+  });
+// toggle-button
+let toggleButton = document.getElementById('toggleButton');
+
+toggleButton.addEventListener('click', function() {
+  toggleButton.classList.toggle('active');
+  if (toggleButton.classList.contains('active')) {
+    window.location.href = 'page2.html'; // URL for the second page
+  } else {
+    window.location.href = 'page2-2.html'; // URL for the first page
+  }
+});
+
+//  next and go back buttons
+
+  
+
+
+
+ 
+
+  
