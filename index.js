909c38ab03e6378e24de83bb7f228f70edf8bdd5
orekbaby <orekoyafolakemi@gmail.com>
2023-05-29 19:35:25 +0100

    Merge branch 'fixes' into master

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
 
   let nameInput = document.getElementById("name");
     let emailInput = document.getElementById("email");
-    
  
     let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    let cards = document.querySelectorAll(".card"); // Select all card elements
+   
     if (!nameInput.value || !emailPattern.test(emailInput.value)) {
       event.preventDefault(); // Prevent form submission
       alert("Please fill in all required fields and enter a valid email address.");
     }
   });
+  let completedSteps = [];
+
+  // Validate step before allowing navigation
+  function validateStep(step) {
+    // Check if the current step has been completed
+    if (completedSteps.includes(step - 1)) {
+      // Mark the current step as completed
+      completedSteps.push(step);
+      return true; // Allow navigation to the next page
+    } else {
+      alert("Please complete the previous step first.");
+      return false; // Prevent navigation to the next page
+    }
+  }
+
 
 
 // cards function
